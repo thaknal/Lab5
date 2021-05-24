a284e618c9c2c61eacc890d4b9fe6a3b878b21a0
thaknal <krzysiekpnq50@gmail.com>
2021-05-24 03:08:44 +0200

    Zadanie 10

--- src/pl/lublin/wsei/java/cwiczenia/lab5/Kulka.java
+++ src/pl/lublin/wsei/java/cwiczenia/lab5/Kulka.java
@@ -6,45 +6,54 @@ import javafx.scene.paint.Color;
 
 public class Kulka {
 
+    private double radius;
     private static final double R = 10;
-    private double ySpeed;
-    private double xSpeed;
-    private double xPos;
-    private double yPos;
-    private Color color;
+    protected double ySpeed;
+    protected double xSpeed;
+    protected double xPos;
+    protected double yPos;
+    protected Color color;
 
-    public Color getColor(){
-        return color;
-    }
-
-    public void setColor(Color color){
-        this.color = color;
-    }
-
-    Kulka(double xPos, double yPos, double xSpeed, double ySpeed, Color whitesmoke){
+    Kulka(double xPos, double yPos, double xSpeed, double ySpeed) {
         this.xPos = xPos;
         this.yPos = yPos;
         this.xSpeed = xSpeed;
         this.ySpeed = ySpeed;
-        color = Color.WHITESMOKE;
+        this.color = Color.WHITESMOKE;
+        this.radius = R;
+    }
+
+    Kulka(double xPos, double yPos, double xSpeed, double ySpeed, Color color) {
+        this(xPos, yPos, xSpeed, ySpeed);
+        this.color = color;
+    }
+
+    Kulka(double xPos, double yPos, double xSpeed, double ySpeed, double radius, Color color) {
+        this(xPos, yPos, xSpeed, ySpeed);
+        this.color = color;
+        this.radius = radius;
     }
 
     public void checkBoundaryCollision(double xLeft, double yTop, double xRight, double yBottom) {
-        if ((xPos - R <= xLeft) || (xPos + R >= xRight)) {
-            xSpeed =- xSpeed;
-        }
-        if ((yPos - R <= yTop) || (yPos + R >= yBottom)) {
-            ySpeed =- ySpeed;
-        }
+        if ((xPos - radius <= xLeft) || (xPos + radius >= xRight)) xSpeed = -xSpeed;
+        if ((yPos - radius <= yTop) || (yPos + radius >= yBottom)) ySpeed = -ySpeed;
     }
 
-    public void draw(GraphicsContext gc){
-        gc.setFill(Color.WHITESMOKE);
-        gc.fillOval(xPos - R, yPos - R, 2 * R, 2 * R);
+    public void draw(GraphicsContext gc) {
+        gc.setFill(color);
+        gc.fillOval(xPos - radius, yPos - radius, 2 * radius, 2 * radius);
     }
 
-    public void update(){
+    public void update() {
         xPos += xSpeed;
         yPos += ySpeed;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
     }
 }
--- src/pl/lublin/wsei/java/cwiczenia/lab5/Pong1.java
+++ src/pl/lublin/wsei/java/cwiczenia/lab5/Pong1.java
@@ -19,89 +19,72 @@ public class Pong1 extends Application {
     private static final double HEIGHT = 600;
     private static final double WIDTH = 800;
     private static final double MARGIN = 50;
-    private static final double ARENAWIDTH = WIDTH - 2 * MARGIN;
-    private static final double ARENAHEIGHT = HEIGHT - 2 * MARGIN;
+    private static final double ARENAWIDTH = WIDTH - 2*MARGIN;
+    private static final double ARENAHEIGHT = HEIGHT - 2*MARGIN;
     private static final double ARENAX1 = MARGIN;
     private static final double ARENAY1 = MARGIN;
     private static final double ARENAX2 = ARENAX1 + ARENAWIDTH;
     private static final double ARENAY2 = ARENAY1 + ARENAHEIGHT;
-    private static final int R = 10;
+
+    private static final int LICZBAKULEK = 10;
+    private Kulka[] kulki = new Kulka[LICZBAKULEK];
+
+
 
     @Override
     public void start(Stage stage) {
         Canvas canvas = new Canvas(WIDTH, HEIGHT);
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
-        Timeline t = new Timeline(new KeyFrame(Duration.millis(40), e -> run(gc)));
-
+        Timeline t = new Timeline(new KeyFrame(Duration.millis(100), e -> run(gc)));
         t.setCycleCount(Timeline.INDEFINITE);
 
-        stage.setTitle("Kulki!");
+        stage.setTitle("Kulki");
         stage.setScene(new Scene(new StackPane(canvas)));
         stage.show();
 
-        initKule();
+        initKula();
         t.play();
-    }
-
-    private static final int LICZBAKULEK = 10;
-    private double[] x = new double[LICZBAKULEK];
-    private double[] y = new double[LICZBAKULEK];
-    private double[] vx = new double[LICZBAKULEK];
-    private double[] vy = new double[LICZBAKULEK];
-    private Kulka[] kulki = new Kulka[LICZBAKULEK];
-
-
-
-    private void initKule() {
-        Random lott = new Random();
-
-        for (int i = 0; i < LICZBAKULEK; i++)
-        {
-            kulki[i] = new Kulka(
-                    lott.nextDouble() * ARENAWIDTH + ARENAX1,
-                    lott.nextDouble() * ARENAHEIGHT + ARENAY1,
-                    5+lott.nextDouble() * 20,
-                    5+lott.nextDouble() * 20,
-                     Color.WHITESMOKE);
-        }
-    }
-
-    private void run(GraphicsContext gc) {
-
-        initKule();
-        gc.setFill(Color.BLACK);
-        gc.fillRect(ARENAX1, ARENAY1, ARENAWIDTH, ARENAHEIGHT);
-
-
-        {
-            for (int i =0; i< LICZBAKULEK; i++) {
-
-                kulki[i].checkBoundaryCollision(ARENAX1, ARENAY1, ARENAX2, ARENAY2);
-                kulki[i].update();
-                kulki[i].draw(gc);
-            }
-        }
-
-        for (int i = 0; i < LICZBAKULEK; i++)
-        {
-            x[i] += vx[i];
-            y[i] += vy[i];
-        }
-
-        for (int i = 0; i < LICZBAKULEK; i++)
-        {
-            gc.setFill(Color.WHITESMOKE);
-            gc.fillOval(x[i]-R, y[i]-R, 2*R, 2*R);
-        }
-
-        gc.setFill(Color.WHITESMOKE);
-        gc.fillOval(ARENAX1 + ARENAWIDTH / 2, ARENAY1 + ARENAHEIGHT / 2, 2 * R, 2 * R);
-
     }
 
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    private void initKula(){
+        Random lott = new Random();
+        for (int i = 0; i < LICZBAKULEK/2; i++){
+            kulki[i] = new Kulka(
+                    lott.nextDouble() * ARENAWIDTH + ARENAX1,
+                    lott.nextDouble() * ARENAHEIGHT + ARENAY1,
+                    5 + lott.nextDouble() * 20,
+                    5 + lott.nextDouble() * 20,
+                    10,
+                    Color.WHITESMOKE);
+        }
+        for (int i = LICZBAKULEK/2; i < LICZBAKULEK; i++){
+            kulki[i] = new Rugby(
+                    lott.nextDouble() * ARENAWIDTH + ARENAX1,
+                    lott.nextDouble() * ARENAHEIGHT + ARENAY1,
+                    5 + lott.nextDouble() * 20,
+                    5 + lott.nextDouble() * 20,
+                    10,
+                    15,
+                    2);
+        }
+    }
+
+    private void run(GraphicsContext gc){
+        gc.setFill(Color.BLACK);
+        gc.fillRect(ARENAX1, ARENAY1, ARENAWIDTH, ARENAHEIGHT);
+
+        for (int i = 0; i < LICZBAKULEK; i++){
+            kulki[i].checkBoundaryCollision(ARENAX1, ARENAY1, ARENAX2, ARENAY2);
+
+            kulki[i].update();
+
+            kulki[i].draw(gc);
+        }
     }
 }
